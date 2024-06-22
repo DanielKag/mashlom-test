ea8dcb538b16214200be84268e85266377d7320d
Daniel Kagan <danielk@wix.com>
2024-06-22 11:00:13 +0300

    base

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/mashlom-test/',
   css: {
     modules: {
       // Optional: Custom CSS modules configuration
