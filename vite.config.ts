7281fe20677a3a2456ffa497a4627f82d2827a01
Daniel Kagan <danielk@wix.com>
2024-06-22 14:36:18 +0300

    remove base and homepage

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/mashlom-test/',
   css: {
     modules: {
       // Optional: Custom CSS modules configuration
